2524c5ccb43e9b3ba0ddf5f44a4dced51b2fdf05
Mr.Online <yasanghani7446@gmail.com>
2020-04-16 16:55:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-//Require discord.js
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
-//create a new client using new login
-const client = new Discord.Client;
+const client = new Discord.Client();
 
-//Constants Array
-const strArr = ["ready","reconnecting","message","!hi","!help"];
-//Display message when bot comes online
-client.on(strArr[0],()=>{
-    console.log(`Logged in as ${client.user.tag}!`)
+ 
+
+client.on('ready', () => {
+
+    console.log('Logged in as ${client.user.tag}!');
+
 });
 
-//Display message when reconnecting
-client.on(strArr[1],()=>{
-    console.log(`This bot is reconnecting: ${client.user.tag}!`)
-});
+ 
 
-//check for new messages
-client.on(strArr[2], msg => {
+client.on('message', message => {
     //converts new messages into lowecase
-    const msgLowered = msg.content.toLowerCase();
+    const msgLowered = message.content.toLowerCase();
     // compares to find if certain command is fired or not
-    if (msgLowered === strArr[3]) {
+    if (msgLowered === "!hi") {
         msg.reply("Yo Hi! Let's play lol :laughing:");
     }
-    else if(msgLowered === strArr[4]) {
+    else if(msgLowered === "!help") {
         msg.reply("Currently I have 2 commands, !hi !help");
     }
 });
 
-//use token(password)
-client.login(process.env.BOT_TOKEN);
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret

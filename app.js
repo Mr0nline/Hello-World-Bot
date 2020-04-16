67566e52dc8108276939a88441e83309760e665f
Mr.Online <yasanghani7446@gmail.com>
2020-04-16 17:51:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ client.on(strArr[2], msg => {
     else if(msgLowered === strArr[4]) {
         msg.reply("Currently I have 3 commands, '!hi' '!help' '!build hayabusa'");
     }
+    else if(msgLowered === "raghu") {
+        msg.reply("LOL :joy:");
+    }
     else if(msgLowered === "!build hayabusa") {
         msg.reply("Here is the build for Hayabusa.\nThis build suggested by <@!331736432957718529> \nHayabusa is assassin hero which is very capable for ganking and pushing.\n<:raptor_machate:700311879779876874> Core jungling item, Increse stacks as fast as you can.\n<:warrior_boot:700311881692348427> Boot for speed and defence, Prioritize this boot before building whole raptor machate.\n<:blade_of_heptaseas:700311877581930516> Core ganking item, with the passive you can deal pretty much damage.\n<:endless_battle:700311878953336852> All round item to give you sustainability.\n<:blade_of_despair:700311821764001932> Increase base and skill damage\n<:queen_wing:700311879683145819> Increasing HP and lifesteal for late game.");
     }
